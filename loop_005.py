0b19cfa7d36b12cf0b136c2cdcbcbaa1385aa359
makusapta <saptarshig370@gmail.com>
2025-02-06 20:31:10 +0530

    loop Using Python

---
+++ loop_005.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(input("Enter a number: "))
+    for i in range(n):
+        print(i ** 2)
+# n = int(input("Enter a number: "))
+# for i in range(n):
+#     print(i ** 2)
